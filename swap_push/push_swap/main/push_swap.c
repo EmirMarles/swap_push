7bfb0ea76f1a65a2b5342516cf0de4ae770fa722
EmirMarles <emirmarles90@gmail.com>
2025-04-27 18:44:54 +0200

    Add files via upload

--- swap_push/push_swap/main/push_swap.c
+++ swap_push/push_swap/main/push_swap.c
@@ -6,11 +6,11 @@
 /*   By: emarles <emarles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 14:39:02 by emarles           #+#    #+#             */
-/*   Updated: 2025/04/23 17:47:44 by emarles          ###   ########.fr       */
+/*   Updated: 2025/04/27 16:45:10 by emarles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../includes/push_swap.h"
 
 int    ft_is_string(char *word);
 
@@ -26,7 +26,7 @@ int    ft_is_string(char *word);
     // Pushswap functions:
     // 1) pa, pb, ra, rb, etc.
 
-static void    sort_stack(t_stack* a, t_stack* b)
+void    sort_stack(t_stack* a, t_stack* b)
 {
     if (a->size == 2)
         sort_two(a, b);
@@ -40,63 +40,9 @@ static void    sort_stack(t_stack* a, t_stack* b)
         radix_sort(a, b);
 }
 
-void    ft_handle_input(int argc, char **argv, t_stack *a)
-{
-    t_node  *new_node;
-    t_node  *head;
-    t_node  *current;
-    
-    head = NULL;
-    current = NULL;
-    argc--;
-    while (argc > 0)
-    {
-        if (ft_is_string(argv[argc]))
-        {
-            write(1, "Error\n", 7);
-            exit(EXIT_FAILURE);
-        }
-        new_node = malloc(sizeof(t_node));
-        if (!new_node)  
-        {
-            write(1, "Malloc failure\n", 16);
-            exit(EXIT_FAILURE);
-        }
-        new_node->number = ft_atoi(argv[argc]);
-        new_node->next = NULL;
-        if (!head) // start of the list;
-        {
-            head = new_node;
-            current = head;
-        }
-        else 
-        {
-            current->next = new_node;
-            current = current->next;
-        }
-        argc--;
-        // printf("Added number in the list: %d\n", new_node->number);
-    }
-    a->node = head; // head should always be saved in a separate value;
-}
-
-int    ft_is_string(char *word)
-{ 
-    int i;
-
-    i = 0;
-    while (word[i] != '\0')
-    {
-        if (ft_isalpha(word[i]))
-            return (1);
-        i++;
-    }
-    return (0);
-}
-
 int main(int argc, char* argv[])
 {
-    t_stack  *a; //head
+    t_stack  *a;
     t_stack  *b;
     int i;
 
@@ -111,7 +57,7 @@ int main(int argc, char* argv[])
         return (0);
     }
     ft_handle_input(argc, argv, a);
-    t_node *current = a->node;
+    t_node *current = a->head;
     while (current != NULL)
     {
         printf("The current value in the linked list : %i\n", current->number);
@@ -120,7 +66,6 @@ int main(int argc, char* argv[])
     ft_assign_indexes(a, argc - 1);
     sort_stack(a, b);
     // ft_exit_free(a);
-    free(a);
-    free(b);
+    // free everything;
     return (0);
 }
---
+++ swap_push/push_swap/main/push_swap_utils.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pushswap_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emarles <emarles@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/13 15:36:58 by emarles           #+#    #+#             */
+/*   Updated: 2025/04/27 16:37:33 by emarles          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	i;
+	int	sign;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r')
+		i++;
+	while ((str[i] == '-' || str[i] == '+') && (str[i + 1] != '+' && str[i
+				+ 1] != '-'))
+	{
+		if (str[i] == '-')
+			sign = (sign * -1);
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
+
+int	ft_isalpha(int a)
+{
+	if ((a >= 0 && a <= 44) || (a >= 58 && a <= 127))
+		return (1);
+	return (0);
+}
+
+char	*ft_strchr(const char *str, int search_str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == (char)search_str)
+		{
+			return ((char *)&str[i]);
+		}
+		i++;
+	}
+	if (search_str == '\0')
+	{
+		return ((char *)&str[i]);
+	}
+	return (NULL);
+}
+
+int    ft_is_string(char *word)
+{ 
+    int i;
+
+    i = 0;
+    while (word[i] != '\0')
+    {
+        if (ft_isalpha(word[i]))
+            return (1);
+        i++;
+    }
+    return (0);
+}
